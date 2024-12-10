5914726597604c4a35c95e29e55075c5bcd4088c
Eric Seguin <eric@segwin.ca>
2024-12-09 22:13:25 -0500

    feat: day 3 part 2

--- internal/solutions/day3/solution.go
+++ internal/solutions/day3/solution.go
@@ -14,25 +14,51 @@ type Solution struct {
 func (s *Solution) RunToConsole() error {
 	fmt.Print("DAY 3:\n")
 
-	sum, err := SumMuls(s.Memory)
+	sum, err := SumMuls(s.Memory, false)
 	if err != nil {
 		return fmt.Errorf("executing muls in memory: %w", err)
 	}
 
 	fmt.Print("  PART 1:\n")
 	fmt.Printf("    Sum: %d\n", sum)
+
+	withConditionals, err := SumMuls(s.Memory, true)
+	if err != nil {
+		return fmt.Errorf("executing muls in memory with conditionals enabled: %w", err)
+	}
+
+	fmt.Print("  PART 2:\n")
+	fmt.Printf("    Sum with conditionals: %d\n", withConditionals)
+
 	return nil
 }
 
-func SumMuls(memory string) (sum int, err error) {
-	pattern := regexp.MustCompile(`mul\(([1-9][0-9]{0,2}),([1-9][0-9]{0,2})\)`)
-	for _, matches := range pattern.FindAllStringSubmatch(memory, -1) {
-		parsedVals, err := transform.Atois(matches[1], matches[2])
-		if err != nil {
-			// note: this shouldn't happen if the regex is well-formed, but let's be safe
-			return 0, fmt.Errorf("parsing mul values (implementation error): %w", err)
+func SumMuls(memory string, withConditionals bool) (sum int, err error) {
+	pattern := `(mul)\(([1-9][0-9]{0,2}),([1-9][0-9]{0,2})\)`
+	if withConditionals {
+		pattern += `|do\(\)|don't\(\)`
+	}
+	rgx := regexp.MustCompile(pattern)
+
+	mulsEnabled := true
+	allMatches := rgx.FindAllStringSubmatch(memory, -1)
+	for _, matches := range allMatches {
+		switch matches[0] {
+		case "do()":
+			mulsEnabled = true
+		case "don't()":
+			mulsEnabled = false
+		default: // mul
+			if !mulsEnabled {
+				continue
+			}
+			parsedVals, err := transform.Atois(matches[2], matches[3])
+			if err != nil {
+				// note: this shouldn't happen if the regex is well-formed, but let's be safe
+				return 0, fmt.Errorf("parsing mul values (implementation error): %w", err)
+			}
+			sum += parsedVals[0] * parsedVals[1]
 		}
-		sum += parsedVals[0] * parsedVals[1]
 	}
 	return sum, nil
 }
--- internal/solutions/day3/solution_test.go
+++ internal/solutions/day3/solution_test.go
@@ -13,22 +13,29 @@ func TestSumMuls(t *testing.T) {
 
 	tests := map[string]struct {
 		// inputs
-		memory string
+		memory           string
+		withConditionals bool
 
 		// outputs
 		expected    int
 		expectedErr error
 	}{
-		"ok: day's example": {
-			memory:   `xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))`,
-			expected: 161,
+		// "ok: day's example": {
+		// 	memory:           `xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))`,
+		// 	withConditionals: false,
+		// 	expected:         161,
+		// },
+		"ok: day's example, with conditionals": {
+			memory:           `xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))`,
+			withConditionals: true,
+			expected:         48,
 		},
 	}
 	for name, tt := range tests {
 		t.Run(name, func(t *testing.T) {
 			t.Parallel()
 
-			got, err := day3.SumMuls(tt.memory)
+			got, err := day3.SumMuls(tt.memory, tt.withConditionals)
 			require.ErrorIs(t, err, tt.expectedErr)
 			assert.Equal(t, tt.expected, got)
 		})
