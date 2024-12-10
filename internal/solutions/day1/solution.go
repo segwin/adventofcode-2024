39817789cf473e9e8c6aab6bcff33401366ee78b
Eric Seguin <eric@segwin.ca>
2024-12-09 19:50:45 -0500

    feat: implement day 1 part 2

--- internal/solutions/day1/solution.go
+++ internal/solutions/day1/solution.go
@@ -16,25 +16,38 @@ type Solution struct {
 }
 
 func (s *Solution) RunToConsole() error {
+	fmt.Print("DAY 1:\n")
+
 	totalDistance, err := s.TotalDistance()
 	if err != nil {
 		return fmt.Errorf("computing total distance: %w", err)
 	}
 
-	fmt.Printf("Total distance: %d\n", totalDistance)
+	fmt.Print("  PART 1:\n")
+	fmt.Printf("    Total distance: %d\n", totalDistance)
+
+	similarity, err := s.Similarity()
+	if err != nil {
+		return fmt.Errorf("computing total distance: %w", err)
+	}
+
+	fmt.Print("  PART 2:\n")
+	fmt.Printf("    Similarity score: %d\n", similarity)
 	return nil
 }
 
 func (s *Solution) TotalDistance() (int, error) {
-	if len(s.Left) != len(s.Right) {
-		return 0, fmt.Errorf("%w (left = %d, right = %d)", ErrMismatchedLens, len(s.Left), len(s.Right))
+	if err := s.validateInputs(); err != nil {
+		return 0, err
 	}
 
-	slices.Sort(s.Left)
-	slices.Sort(s.Right)
+	// sort lists to compare lowest values pairwise
+	left := slices.Sorted(slices.Values(s.Left))
+	right := slices.Sorted(slices.Values(s.Right))
+
 	totalDistance := 0
-	for i := range s.Left {
-		low, high := s.Left[i], s.Right[i]
+	for i := range left {
+		low, high := left[i], right[i]
 		if high < low {
 			low, high = high, low
 		}
@@ -42,4 +55,28 @@ func (s *Solution) TotalDistance() (int, error) {
 	}
 
 	return totalDistance, nil
+}
+
+func (s *Solution) Similarity() (int, error) {
+	if err := s.validateInputs(); err != nil {
+		return 0, err
+	}
+
+	score := 0
+	for _, lv := range s.Left {
+		for _, rv := range s.Right {
+			if lv == rv {
+				score += lv
+			}
+		}
+	}
+
+	return score, nil
+}
+
+func (s *Solution) validateInputs() error {
+	if len(s.Left) != len(s.Right) {
+		return fmt.Errorf("%w (left = %d, right = %d)", ErrMismatchedLens, len(s.Left), len(s.Right))
+	}
+	return nil
 }
--- internal/solutions/day1/solution_test.go
+++ internal/solutions/day1/solution_test.go
@@ -44,3 +44,40 @@ func TestSolution_TotalDistance(t *testing.T) {
 		})
 	}
 }
+
+func TestSolution_Similarity(t *testing.T) {
+	t.Parallel()
+
+	tests := map[string]struct {
+		// inputs
+		solution day1.Solution
+
+		// outputs
+		expected    int
+		expectedErr error
+	}{
+		"error: mismatched left/right lengths": {
+			solution: day1.Solution{
+				Left:  []int{1, 2, 3},
+				Right: []int{4, 5},
+			},
+			expectedErr: day1.ErrMismatchedLens,
+		},
+		"ok: day's example": {
+			solution: day1.Solution{
+				Left:  []int{3, 4, 2, 1, 3, 3},
+				Right: []int{4, 3, 5, 3, 9, 3},
+			},
+			expected: 31,
+		},
+	}
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			got, err := tt.solution.Similarity()
+			require.ErrorIs(t, err, tt.expectedErr)
+			assert.Equal(t, tt.expected, got)
+		})
+	}
+}
