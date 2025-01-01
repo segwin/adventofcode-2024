ea981e26152eceec66d6838eb0e7ac839e248ed9
Eric Seguin <eric@segwin.ca>
2025-01-01 16:54:01 -0500

    chore: add console boilerplate for all remaining solutions

--- internal/solutions/day24/solution.go
+++ internal/solutions/day24/solution.go
@@ -7,6 +7,11 @@ type Solution struct{}
 func (s *Solution) RunToConsole() error {
 	fmt.Print("DAY 24:\n")
 
-	// TODO
+	fmt.Print("  PART 1:\n")
+	fmt.Printf("    TODO\n")
+
+	fmt.Print("  PART 2:\n")
+	fmt.Printf("    TODO\n")
+
 	return nil
 }
--- internal/solutions/day25/solution.go
+++ internal/solutions/day25/solution.go
@@ -7,6 +7,11 @@ type Solution struct{}
 func (s *Solution) RunToConsole() error {
 	fmt.Print("DAY 25:\n")
 
-	// TODO
+	fmt.Print("  PART 1:\n")
+	fmt.Printf("    TODO\n")
+
+	fmt.Print("  PART 2:\n")
+	fmt.Printf("    TODO\n")
+
 	return nil
 }
