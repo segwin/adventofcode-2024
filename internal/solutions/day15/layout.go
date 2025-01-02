18c72bc608f674b0188fefa67fa467320af840dd
Eric Seguin <eric@segwin.ca>
2025-01-01 23:43:34 -0500

    feat: implement day 15 part 2

--- internal/solutions/day15/layout.go
+++ internal/solutions/day15/layout.go
@@ -4,10 +4,14 @@ import "github.com/segwin/adventofcode-2024/internal/solutions/map2d"
 
 type Tile byte
 
+func (t Tile) String() string { return string(t) }
+
 const (
 	Empty = '.'
 	Wall  = '#'
 	Box   = 'O'
+	BoxL  = '['
+	BoxR  = ']'
 	Robot = '@'
 )
 
--- internal/solutions/day15/solution.go
+++ internal/solutions/day15/solution.go
@@ -15,19 +15,33 @@ type Solution struct {
 func (s *Solution) RunToConsole() error {
 	fmt.Print("DAY 15:\n")
 
-	finalLayout, _ := Resolve(s.Layout, s.Instructions...)
-	boxes := finalLayout.FindAll(Box)
+	// part 1
+	finalLayout, err := Resolve(s.Layout, s.Instructions...)
+	if err != nil {
+		return fmt.Errorf("resolving instructions in regular layout: %w", err)
+	}
 
 	gpsSum := 0
-	for _, box := range boxes {
+	for _, box := range finalLayout.FindAll(Box) {
 		gpsSum += 100*box.Y + box.X
 	}
 
 	fmt.Print("  PART 1:\n")
 	fmt.Printf("    Box GPS sum: %d\n", gpsSum)
 
+	// part 2
+	finalLayout, err = Resolve(WidenWarehouse(s.Layout), s.Instructions...)
+	if err != nil {
+		return fmt.Errorf("resolving instructions in wide layout: %w", err)
+	}
+
+	gpsSum = 0
+	for _, box := range finalLayout.FindAll(BoxL) {
+		gpsSum += 100*box.Y + box.X
+	}
+
 	fmt.Print("  PART 2:\n")
-	fmt.Printf("    TODO\n")
+	fmt.Printf("    Box GPS sum in wide warehouse: %d\n", gpsSum)
 
 	return nil
 }
@@ -46,29 +60,82 @@ func Resolve(layout Layout, instructions ...map2d.Direction) (Layout, error) {
 	return layout, nil
 }
 
-func resolveOne(curLayout Layout, curRobot map2d.Position, instruction map2d.Direction) (newLayout Layout, newRobot map2d.Position) {
-	newRobot = curRobot.Move(instruction, 1)
+func resolveOne(layout Layout, robot map2d.Position, instruction map2d.Direction) (Layout, map2d.Position) {
+	// find all boxes in the way in order to move them too
+	movementFront := map[map2d.Position]struct{}{robot: {}} // position of all objects at the front of computed movement
+	tilesToMove := map[map2d.Position]Tile{robot: Robot}
+	for len(movementFront) != 0 {
+		newMovementFront := map[map2d.Position]struct{}{}
+		for frontPos := range movementFront {
+			nextPos := frontPos.Move(instruction, 1)
+			nextTile, _ := layout.Get(nextPos) // can never go out of bounds since there's an impassable wall
 
-	nextPos := newRobot
-	nextTile, _ := curLayout.Get(nextPos) // next tile should never be out of bounds
+			// handle other half of box in extra-wide map
+			switch nextTile {
+			case Wall:
+				return layout, robot // stop immediately: something is hitting a wall
+			case Empty:
+				continue // ok: nothing but air ahead
+			}
 
-	var nextBoxes []map2d.Position
-	for nextTile == Box {
-		nextBoxes = append(nextBoxes, nextPos)
-		nextPos = nextPos.Move(instruction, 1)
-		nextTile, _ = curLayout.Get(nextPos) // next tile should never be out of bounds
+			// tile ahead is a box: add it to the next iteration's movement front
+			newMovementFront[nextPos] = struct{}{}
+			tilesToMove[nextPos] = nextTile
+
+			if instruction == map2d.East() || instruction == map2d.West() {
+				continue // stop here: wide (L/R) boxes only require joining in vertical movement
+			}
+
+			// moving vertically: ensure paired L/R boxes are moved together
+			switch nextTile {
+			case BoxL:
+				joinedPos := nextPos.Move(map2d.East(), 1)
+				tilesToMove[joinedPos] = BoxR
+				newMovementFront[joinedPos] = struct{}{}
+			case BoxR:
+				joinedPos := nextPos.Move(map2d.West(), 1)
+				tilesToMove[joinedPos] = BoxL
+				newMovementFront[joinedPos] = struct{}{}
+			}
+		}
+		movementFront = newMovementFront
 	}
 
-	if nextTile == Wall {
-		return curLayout, curRobot // do nothing: can't move walls
+	// ok: move all tiles to their new positions
+	for pos := range tilesToMove {
+		layout.Map = layout.With(pos, Empty)
+	}
+	for pos, tile := range tilesToMove {
+		layout.Map = layout.With(pos.Move(instruction, 1), tile)
 	}
 
-	// found an empty tile: move robot ahead & move any boxes that were in the way
-	curLayout.Map = curLayout.With(curRobot, Empty)
-	curLayout.Map = curLayout.With(newRobot, Robot)
-	for _, curBox := range nextBoxes {
-		curLayout.Map = curLayout.With(curBox.Move(instruction, 1), Box)
-	}
+	return layout, robot.Move(instruction, 1)
+}
 
-	return curLayout, newRobot
+// WidenWarehouse returns the "wide" version of this layout, with twice as many columns and tiles
+// adjusted using the following rules:
+//
+//   - # => ##
+//   - O => []
+//   - . => ..
+//   - @ => @.
+func WidenWarehouse(layout Layout) (wideLayout Layout) {
+	wideLayout.Map = make([][]Tile, len(layout.Map)) // same number of rows
+	for i, row := range layout.Map {
+		wideLayout.Map[i] = make([]Tile, 2*len(row)) // twice as many columns
+		for j, tile := range row {
+			switch tile {
+			case Robot: // @ => @.
+				wideLayout.Map[i][2*j+0] = Robot
+				wideLayout.Map[i][2*j+1] = Empty
+			case Box: // O => []
+				wideLayout.Map[i][2*j+0] = BoxL
+				wideLayout.Map[i][2*j+1] = BoxR
+			default: // everything else: tile is doubled up
+				wideLayout.Map[i][2*j+0] = tile
+				wideLayout.Map[i][2*j+1] = tile
+			}
+		}
+	}
+	return wideLayout
 }
